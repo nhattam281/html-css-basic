f9b64597921bce167b07a97d8a8111ec417ee5bb
Nhat Tam <nhattamlh281@gmail.com>
2023-01-04 23:34:48 +0700

    responsive

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const modal = document.querySelector('.js-modal');
 const modalContainer = document.querySelector('.js-modal-container');
 const modalClose = document.querySelector('.js-modal-close');
 
+//responsive
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
+var headerHeight = header.clientHeight;
+var menuItems = document.querySelectorAll('.nav li a[href*="#"]');
+console.log(menuItems);
+
 // Hàm mở hiển thị modal khi nhấn button (thêm class open vào thẻ modal)
 function showModalBuy() {
     modal.classList.add('open');
@@ -25,3 +32,30 @@ modal.addEventListener('click', removeModalBuy);
 modalContainer.addEventListener('click', function (event) {
     event.stopPropagation();
 });
+
+//header responsive
+// Đóng mở menu
+mobileMenu.onclick = function () {
+    var isClosed = header.clientHeight === headerHeight;
+    if (isClosed) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+    }
+};
+
+// tự động đóng khi nhấn vào menu
+
+for (i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    menuItem.onclick = function (Event) {
+        var isParentMenu =
+            this.nextElementSibling &&
+            this.nextElementSibling.classList.contains('subnav');
+        if (isParentMenu) {
+            event.preventDefault();
+        } else {
+            header.style.height = null;
+        }
+    };
+}
